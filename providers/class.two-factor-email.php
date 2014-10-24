898d270d33c530acc8599df7a64d4909e444ef98
George Stephanis <george@stephanis.info>
2014-10-24 11:55:49 -0400

    Email: Add input field.

--- providers/class.two-factor-email.php
+++ providers/class.two-factor-email.php
@@ -17,6 +17,12 @@ class Two_Factor_Email extends Two_Factor_Provider {
 
 	function authentication_page( $user ) {
 		require_once( ABSPATH .  '/wp-admin/includes/template.php' );
+		?>
+		<p>
+			<label for="authcode"><?php esc_html_e( 'Verification Code:' ); ?></label>
+			<input type="tel" name="twostep-authcode" id="authcode" class="input" value="" size="20" pattern="[0-9]*" />
+		</p>
+		<?php
 		submit_button( __( 'Log In', 'two-factor' ) );
 	}
 
