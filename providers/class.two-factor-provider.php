6383d5b404cabb3431e30c256a0b2016263f953a
George Stephanis <george@stephanis.info>
2014-10-24 08:47:35 -0400

    Create `get_code()` code in parent class.

--- providers/class.two-factor-provider.php
+++ providers/class.two-factor-provider.php
@@ -18,4 +18,19 @@ abstract class Two_Factor_Provider {
 	 */
 	abstract function validate_authentication_page();
 
+	/**
+	 * Generate a random six-digit string to send out as an auth code.
+	 */
+	function get_code( $length = 8, $chars = '1234567890' ) {
+		$code = '';
+		if ( ! is_array( $chars ) ) {
+			$chars = str_split( $chars );
+		}
+		for ( $i = 0; $i < $length; $i++ ) {
+			$key = array_rand( $chars );
+			$code += $chars[ $key ];
+		}
+		return $chars;
+	}
+
 }
