1a195738b1648fcd48ed6f5ff1401419433acaef
Derek Herman <derek@valendesigns.com>
2015-07-17 11:22:11 -0700

    add @todo

--- class.application-passwords.php
+++ class.application-passwords.php
@@ -135,6 +135,7 @@ class Application_Passwords {
 
 			<?php
 				require( dirname( __FILE__ ) . '/class.application-passwords-list-table.php' );
+				// @todo Isn't this class already loaded in Two_Factor_Core::get_providers()?
 				$application_passwords_list_table = new Application_Passwords_List_Table();
 				$application_passwords_list_table->items = $application_passwords;
 				$application_passwords_list_table->prepare_items();
