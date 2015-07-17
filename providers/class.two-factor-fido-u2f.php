6e9fda7108d99cb3ccf0906be09b78dad8611f3c
Derek Herman <derek@valendesigns.com>
2015-07-17 12:19:24 -0700

    FIDO U2f docs

--- providers/class.two-factor-fido-u2f.php
+++ providers/class.two-factor-fido-u2f.php
@@ -1,7 +1,18 @@
 <?php
-
+/**
+ * Class for creating a FIDO Universal 2nd Factor provider.
+ *
+ * @since 0.1-dev
+ *
+ * @package Two_Factor
+ */
 class Two_Factor_Fido_U2f extends Two_Factor_Provider {
 
+	/**
+	 * Ensures only one instance of this class exists in memory at any one time.
+	 *
+	 * @since 0.1-dev
+	 */
 	static function get_instance() {
 		static $instance;
 		$class = __CLASS__;
@@ -11,11 +22,32 @@ class Two_Factor_Fido_U2f extends Two_Factor_Provider {
 		return $instance;
 	}
 
+	/**
+	 * Returns the name of the provider.
+	 *
+	 * @since 0.1-dev
+	 */
 	function get_label() {
 		return _x( 'FIDO U2f', 'Provider Label', 'two-factor' );
 	}
 
+	/**
+	 * Prints the form that prompts the user to authenticate.
+	 *
+	 * @since 0.1-dev
+	 *
+	 * @param WP_User $user WP_User object of the logged-in user.
+	 */
 	function authentication_page( $user ) {}
+
+	/**
+	 * Validates the users input token.
+	 *
+	 * @since 0.1-dev
+	 *
+	 * @param WP_User $user WP_User object of the logged-in user.
+	 * @return boolean
+	 */
 	function validate_authentication( $user ) {}
 
 }
