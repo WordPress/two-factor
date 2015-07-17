e3f9ee18e76d5ca689b8189439ccd86784eb9678
Derek Herman <derek@valendesigns.com>
2015-07-17 12:16:26 -0700

    totp docs

--- providers/class.two-factor-totp.php
+++ providers/class.two-factor-totp.php
@@ -1,7 +1,18 @@
 <?php
-
+/**
+ * Class for creating a Time Based One-Time Password provider.
+ *
+ * @since 0.1-dev
+ *
+ * @package Two_Factor
+ */
 class Two_Factor_Totp extends Two_Factor_Provider {
 
+	/**
+	 * Ensures only one instance of this class exists in memory at any one time.
+	 *
+	 * @since 0.1-dev
+	 */
 	static function get_instance() {
 		static $instance;
 		$class = __CLASS__;
@@ -11,12 +22,32 @@ class Two_Factor_Totp extends Two_Factor_Provider {
 		return $instance;
 	}
 
-
-	function get_label() {
+	/**
+	 * Returns the name of the provider.
+	 *
+	 * @since 0.1-dev
+	 */
+	public function get_label() {
 		return _x( 'Time Based One-Time Password (Google Authenticator)', 'Provider Label', 'two-factor' );
 	}
 
-	function authentication_page( $user ) {}
-	function validate_authentication( $user ) {}
+	/**
+	 * Prints the form that prompts the user to authenticate.
+	 *
+	 * @since 0.1-dev
+	 *
+	 * @param WP_User $user WP_User object of the logged-in user.
+	 */
+	public function authentication_page( $user ) {}
+
+	/**
+	 * Validates the users input token.
+	 *
+	 * @since 0.1-dev
+	 *
+	 * @param WP_User $user WP_User object of the logged-in user.
+	 * @return boolean
+	 */
+	public function validate_authentication( $user ) {}
 
 }
