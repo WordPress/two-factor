61bdadd01abcf7cacf43ff942382808119b2c87a
Derek Herman <derek@valendesigns.com>
2015-07-17 12:06:18 -0700

    add provider docs

--- providers/class.two-factor-dummy.php
+++ providers/class.two-factor-dummy.php
@@ -1,7 +1,18 @@
 <?php
-
+/**
+ * Class for creating a dummy provider.
+ *
+ * @since 0.1-dev
+ *
+ * @package Two_Factor
+ */
 class Two_Factor_Dummy extends Two_Factor_Provider {
 
+	/**
+	 * Ensures only one instance of this class exists in memory at any one time.
+	 *
+	 * @since 0.1-dev
+	 */
 	static function get_instance() {
 		static $instance;
 		$class = __CLASS__;
@@ -11,10 +22,22 @@ class Two_Factor_Dummy extends Two_Factor_Provider {
 		return $instance;
 	}
 
+	/**
+	 * Returns the name of the provider.
+	 *
+	 * @since 0.1-dev
+	 */
 	function get_label() {
 		return _x( 'Dummy Method', 'Provider Label', 'two-factor' );
 	}
 
+	/**
+	 * Prints the form that prompts the user to authenticate.
+	 *
+	 * @since 0.1-dev
+	 *
+	 * @param WP_User $user WP_User object of the logged-in user.
+	 */
 	function authentication_page( $user ) {
 		require_once( ABSPATH .  '/wp-admin/includes/template.php' );
 		?>
@@ -23,6 +46,16 @@ class Two_Factor_Dummy extends Two_Factor_Provider {
 		submit_button( __( 'Yup.', 'two-factor' ) );
 	}
 
+	/**
+	 * Validates the users input token.
+	 *
+	 * In this class we just return true.
+	 *
+	 * @since 0.1-dev
+	 *
+	 * @param WP_User $user WP_User object of the logged-in user.
+	 * @return boolean
+	 */
 	function validate_authentication( $user ) {
 		return true;
 	}
--- providers/class.two-factor-provider.php
+++ providers/class.two-factor-provider.php
@@ -1,45 +1,65 @@
 <?php
-
+/**
+ * Abstract class for creating two factor authentication providers.
+ *
+ * @since 0.1-dev
+ *
+ * @package Two_Factor
+ */
 abstract class Two_Factor_Provider {
 
+	/**
+	 * Class constructor.
+	 *
+	 * @since 0.1-dev
+	 */
 	protected function __construct() {
 		return $this;
 	}
 
 	/**
-	 * Returns the name of the method.
+	 * Returns the name of the provider.
+	 *
+	 * @since 0.1-dev
 	 *
 	 * @return string
 	 */
 	abstract function get_label();
 
 	/**
-	 * Prints the name of the method.
+	 * Prints the name of the provider.
+	 *
+	 * @since 0.1-dev
 	 */
-	function print_label() {
+	public function print_label() {
 		echo esc_html( $this->get_label() );
 	}
 
 	/**
 	 * Prints the form that prompts the user to authenticate.
 	 *
-	 * @param $user WP_User
+	 * @since 0.1-dev
+	 *
+	 * @param WP_User $user WP_User object of the logged-in user.
 	 */
 	abstract function authentication_page( $user );
 
 	/**
 	 * Validates the users input token.
 	 *
-	 * @param $user WP_User
+	 * @since 0.1-dev
 	 *
+	 * @param WP_User $user WP_User object of the logged-in user.
 	 * @return boolean
 	 */
 	abstract function validate_authentication( $user );
 
 	/**
 	 * Generate a random eight-digit string to send out as an auth code.
+	 *
+	 * @since 0.1-dev
 	 */
-	function get_code( $length = 8, $chars = '1234567890' ) {
+	public function get_code( $length = 8, $chars = '1234567890' ) {
 		$code = '';
 		if ( ! is_array( $chars ) ) {
 			$chars = str_split( $chars );
