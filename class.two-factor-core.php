b2088d0ceb68097d279e7927805e4a7e7889a0b3
Daisuke Takahashi <daisuke@extendwings.com>
2015-09-04 23:25:27 +0900

    Silence CodeSniffer

--- class.two-factor-core.php
+++ class.two-factor-core.php
@@ -63,10 +63,10 @@ class Two_Factor_Core {
 		// FIDO U2F is PHP 5.3+ only.
 		if ( version_compare( PHP_VERSION, '5.3.0', '<' ) ) {
 			unset( $providers['Two_Factor_FIDO_U2F'] );
-			trigger_error( sprintf(
+			trigger_error( esc_html( sprintf(
 				__( 'FIDO U2F is not available because you are using PHP %s. (Requires 5.3 or greater)' ),
 				PHP_VERSION
-			) );
+			) ) );
 		}
 
 		/**
