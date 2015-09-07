cf7adb794e61b3402c04f945dffa751e6ceb03ca
scotchfield <swrittenb@gmail.com>
2015-09-06 22:15:37 -0400

    Wrap library to prevent parse errors on PHP 5.2

--- tests/phpunit/tests/providers/class.two-factor-fido-u2f.php
+++ tests/phpunit/tests/providers/class.two-factor-fido-u2f.php
@@ -21,9 +21,9 @@ class Tests_Two_Factor_FIDO_U2F extends WP_UnitTestCase {
 		}
 
 		try {
-			require_once('includes/Yubico/U2F.php');
+			require_once( 'tests/phpunit/includes/u2f.php' );
 
-			$this->u2f = new u2flib_server\U2F( "http://demo.example.com" );
+			$this->u2f = Yubico_U2F_Wrapper::get( 'http://demo.example.com' );
 
 			$this->provider = Two_Factor_FIDO_U2F::get_instance();
 		} catch ( Exception $e ) {
