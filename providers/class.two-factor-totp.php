1fc04904ad663f66e175755a335edfb5f349b753
Kaspars Dambis <hi@kaspars.net>
2015-12-03 00:09:52 +0200

    Place instructions above the QR code

--- providers/class.two-factor-totp.php
+++ providers/class.two-factor-totp.php
@@ -77,9 +77,9 @@ class Two_Factor_Totp extends Two_Factor_Provider {
 				$key = $this->generate_key();
 				$site_name = get_bloginfo( 'name', 'display' );
 				?>
-				<img src="<?php echo esc_url( $this->get_google_qr_code( $site_name . ':' . $user->user_login, $key, $site_name ) ); ?>" id="two-factor-totp-qrcode" />
-				<p><strong><?php echo esc_html( $key ); ?></strong></p>
 				<p><?php esc_html_e( 'Please scan the QR code or manually enter the key, then enter an authentication code from your app in order to complete setup' ); ?></p>
+				<p><img src="<?php echo esc_url( $this->get_google_qr_code( $site_name . ':' . $user->user_login, $key, $site_name ) ); ?>" id="two-factor-totp-qrcode" /></p>
+				<p><strong><?php echo esc_html( $key ); ?></strong></p>
 				<p>
 					<label for="two-factor-totp-authcode"><?php esc_html_e( 'Authentication Code:' ); ?></label>
 					<input type="hidden" name="two-factor-totp-key" value="<?php echo esc_attr( $key ) ?>" />
