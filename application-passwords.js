56fd04c120ad56ff6a6368e696ad52103f75c443
Daisuke Takahashi <daisuke@extendwings.com>
2016-01-17 05:52:01 +0900

    Fix JSHint Error

--- application-passwords.js
+++ application-passwords.js
@@ -1,5 +1,5 @@
 /* global appPass, wp */
-(function($,appPass){
+(function( $, appPass ) {
 	var $appPassSection   = $( '#application-passwords-section' ),
 		$newAppPassForm   = $appPassSection.find( '.create-application-password' ),
 		$newAppPassField  = $newAppPassForm.find( '.input' ),
@@ -9,7 +9,7 @@
 		tmplNewAppPass    = wp.template( 'new-application-password' ),
 		tmplAppPassRow    = wp.template( 'application-password-row' );
 
-	$newAppPassButton.click( function(e){
+	$newAppPassButton.click( function( e ) {
 		e.preventDefault();
 		var name = $newAppPassField.val();
 
@@ -18,25 +18,25 @@
 			return;
 		}
 
-		$newAppPassField.prop('disabled', true);
-		$newAppPassButton.prop('disabled', true);
+		$newAppPassField.prop( 'disabled', true );
+		$newAppPassButton.prop( 'disabled', true );
 
 		$.ajax( {
-			url        : appPass.root + appPass.namespace + '/application-passwords/' + appPass.user_id + '/add',
-			method     : 'POST',
-			beforeSend : function ( xhr ) {
+			url:        appPass.root + appPass.namespace + '/application-passwords/' + appPass.user_id + '/add',
+			method:     'POST',
+			beforeSend: function( xhr ) {
 				xhr.setRequestHeader( 'X-WP-Nonce', appPass.nonce );
 			},
-			data       : {
-				name : name
+			data:       {
+				name: name
 			}
-		} ).done( function ( response ) {
+		} ).done( function( response ) {
 			$newAppPassField.prop( 'disabled', false ).val('');
 			$newAppPassButton.prop( 'disabled', false );
 
 			$newAppPassForm.after( tmplNewAppPass( {
-				name     : name,
-				password : response.password
+				name:     name,
+				password: response.password
 			} ) );
 
 			$appPassTbody.prepend( tmplAppPassRow( response.row ) );
@@ -45,21 +45,21 @@
 		} );
 	});
 
-	$appPassTbody.on( 'click', '.delete a', function(e){
+	$appPassTbody.on( 'click', '.delete a', function( e ) {
 		e.preventDefault();
 		var $tr  = $( e.target ).closest( 'tr' ),
 			slug = $tr.data( 'slug' );
 
 		$.ajax( {
-			url        : appPass.root + appPass.namespace + '/application-passwords/' + appPass.user_id + '/' + slug,
-			method     : 'DELETE',
-			beforeSend : function ( xhr ) {
+			url:        appPass.root + appPass.namespace + '/application-passwords/' + appPass.user_id + '/' + slug,
+			method:     'DELETE',
+			beforeSend: function( xhr ) {
 				xhr.setRequestHeader( 'X-WP-Nonce', appPass.nonce );
 			}
-		} ).done( function ( response ) {
+		} ).done( function( response ) {
 			if ( response ) {
 				$tr.remove();
 			}
 		} );
-	});
-})(jQuery,appPass);
+	} );
+} )( jQuery, appPass );
