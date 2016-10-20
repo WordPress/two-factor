457209654968dc07d685b814de3444ce50060903
Kaspars Dambis <hi@kaspars.net>
2016-10-20 20:50:28 +0300

    Update sign request params

--- providers/js/fido-u2f-login.js
+++ providers/js/fido-u2f-login.js
@@ -1,13 +1,11 @@
 /* global u2f, u2fL10n */
 ( function( $ ) {
-	setTimeout( function() {
-		window.console.log( 'sign', u2fL10n.request );
+	window.console.log( 'sign', u2fL10n.request );
 
-		u2f.sign( u2fL10n.request, function( data ) {
-			window.console.log( 'Authenticate callback', data );
+	u2f.sign( u2fL10n.request[0].appId, u2fL10n.request[0].challenge, u2fL10n.request, function( data ) {
+		window.console.log( 'Authenticate callback', data );
 
-			$( '#u2f_response' ).val( JSON.stringify( data ) );
-			$( '#loginform' ).submit();
-		} );
-	}, 1000 );
+		$( '#u2f_response' ).val( JSON.stringify( data ) );
+		$( '#loginform' ).submit();
+	} );
 } )( jQuery );
