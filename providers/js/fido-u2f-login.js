c2abdd7c80bc816359bc51fbc5c6f92ddaf9a43f
Kaspars Dambis <hi@kaspars.net>
2016-10-20 21:57:14 +0300

    Ensure we don’t submit the form on U2F error

--- providers/js/fido-u2f-login.js
+++ providers/js/fido-u2f-login.js
@@ -9,7 +9,9 @@
 	u2f.sign( u2fL10n.request[0].appId, u2fL10n.request[0].challenge, u2fL10n.request, function( data ) {
 		window.console.log( 'Authenticate callback', data );
 
-		if ( ! data.errorCode ) {
+		if ( data.errorCode ) {
+			window.console.log( 'Registration Failed', data.errorCode );
+		} else {
 			$( '#u2f_response' ).val( JSON.stringify( data ) );
 			$( '#loginform' ).submit();
 		}
