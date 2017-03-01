c5326322b7c1d62083e9c1edb5f970b15c6e2186
Josh Betz <j@joshbetz.com>
2017-03-01 12:57:36 -0600

    Don't show QR error when totp method is not enabled (#145)
    * Don't show QR error when totp method is not enabled
    
    Fixes #77
    
    * Remove stored TOTP secret if TOTP is disabled
    
    Fixes #86
    
    * Use strict comparison for in_array
    
    ```
    warning - Not using strict comparison for in_array; supply true for third argument. (WordPress.PHP.StrictInArray.MissingTrueStrict)
    ```

--- providers/class.two-factor-totp.php
+++ providers/class.two-factor-totp.php
@@ -105,16 +105,17 @@ class Two_Factor_Totp extends Two_Factor_Provider {
 			check_admin_referer( 'user_two_factor_totp_options', '_nonce_user_two_factor_totp_options' );
 
 			$current_key = get_user_meta( $user_id, self::SECRET_META_KEY, true );
-			// If the key hasn't changed or is invalid, do nothing.
-			if ( ! isset( $_POST['two-factor-totp-key'] ) || $current_key === $_POST['two-factor-totp-key'] || ! preg_match( '/^[' . self::$_base_32_chars . ']+$/', $_POST['two-factor-totp-key'] ) ) {
+			if ( $current_key && ! in_array( 'Two_Factor_Totp', Two_Factor_Core::get_enabled_providers_for_user( $user_id ), true ) ) {
+				delete_user_meta( $user_id, self::SECRET_META_KEY );
+				return;
+			} elseif ( ! isset( $_POST['two-factor-totp-key'] ) || $current_key === $_POST['two-factor-totp-key'] || ! preg_match( '/^[' . self::$_base_32_chars . ']+$/', $_POST['two-factor-totp-key'] ) ) {
+				// If the key hasn't changed or is invalid, do nothing.
 				return false;
 			}
 
 			$notices = array();
 
-			if ( empty( $_POST['two-factor-totp-authcode'] ) ) {
-				$notices['error'][] = __( 'Two Factor Authentication not activated, you must specify authcode to ensure it is properly set up. Please re-scan the QR code and enter the code provided by your application.', 'two-factor' );
-			} else {
+			if ( ! empty( $_POST['two-factor-totp-authcode'] ) ) {
 				if ( $this->is_valid_authcode( $_POST['two-factor-totp-key'], $_POST['two-factor-totp-authcode'] ) ) {
 					if ( ! update_user_meta( $user_id, self::SECRET_META_KEY, $_POST['two-factor-totp-key'] ) ) {
 						$notices['error'][] = __( 'Unable to save Two Factor Authentication code. Please re-scan the QR code and enter the code provided by your application.', 'two-factor' );
