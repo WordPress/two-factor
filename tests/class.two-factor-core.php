8980d90cc612d62f36c84ea11d1a68381b6d6976
Mike Selander <mikeselander@gmail.com>
2018-09-05 12:51:46 -0600

    Split up core class into Force and Core for better separation

--- tests/class.two-factor-core.php
+++ tests/class.two-factor-core.php
@@ -84,48 +84,6 @@ class Test_ClassTwoFactorCore extends WP_UnitTestCase {
 				array( 'Two_Factor_Core', 'register_scripts' )
 			)
 		);
-		$this->assertGreaterThan(
-			0,
-			has_action(
-				'wpmu_options',
-				array( 'Two_Factor_Core', 'force_two_factor_setting_options' )
-			)
-		);
-		$this->assertGreaterThan(
-			0,
-			has_action(
-				'update_wpmu_options',
-				array( 'Two_Factor_Core', 'save_network_force_two_factor_update' )
-			)
-		);
-		$this->assertGreaterThan(
-			0,
-			has_action(
-				'wp_ajax_two_factor_force_form_submit',
-				array( 'Two_Factor_Core', 'handle_force_2fa_submission' )
-			)
-		);
-		$this->assertGreaterThan(
-			0,
-			has_action(
-				'two_factor_ajax_options_update',
-				array( 'Two_Factor_Core', 'user_two_factor_options_update' )
-			)
-		);
-		$this->assertGreaterThan(
-			0,
-			has_action(
-				'parse_request',
-				array( 'Two_Factor_Core', 'maybe_force_2fa_settings' )
-			)
-		);
-		$this->assertGreaterThan(
-			0,
-			has_action(
-				'admin_init',
-				array( 'Two_Factor_Core', 'maybe_force_2fa_settings' )
-			)
-		);
 	}
 
 	/**
@@ -134,7 +92,6 @@ class Test_ClassTwoFactorCore extends WP_UnitTestCase {
 	public function test_register_scripts() {
 		Two_Factor_Core::register_scripts();
 
-		$this->assertTrue( wp_script_is( 'two-factor-form-script', 'registered' ) );
 		$this->assertTrue( wp_style_is( 'user-edit-2fa', 'registered' ) );
 	}
 
@@ -238,124 +195,5 @@ class Test_ClassTwoFactorCore extends WP_UnitTestCase {
 	 */
 	public function test_is_user_using_two_factor_not_logged_in() {
 		$this->assertFalse( Two_Factor_Core::is_user_using_two_factor() );
-	}
-
-	/**
-	 * @covers Two_Factor_Core::maybe_force_2fa_settings
-	 */
-	public function test_maybe_force_2fa_settings_logged_in_wrong_role() {
-		// Set universal value to false.
-		update_site_option( Two_Factor_Core::FORCED_SITE_META_KEY, 0 );
-		// Set role-based value to editors and adminstrators.
-		update_site_option( Two_Factor_Core::FORCED_ROLES_META_KEY, [ 'editor', 'administrator' ] );
-
-		$user = new WP_User( $this->factory->user->create( [ 'role' => 'author' ] ) );
-		wp_set_current_user( $user->ID );
-
-		$this->assertFalse( Two_Factor_Core::maybe_force_2fa_settings() );
-	}
-
-	/**
-	 * @covers Two_Factor_Core::maybe_force_2fa_settings
-	 */
-	public function test_maybe_force_2fa_settings_logged_in_no_requirement() {
-		// Set universal value to false.
-		update_site_option( Two_Factor_Core::FORCED_SITE_META_KEY, 0 );
-
-		$user = new WP_User( $this->factory->user->create() );
-		wp_set_current_user( $user->ID );
-
-		$this->assertFalse( Two_Factor_Core::maybe_force_2fa_settings() );
-	}
-
-	/**
-	 * @covers Two_Factor_Core::maybe_force_2fa_settings
-	 */
-	public function test_maybe_force_2fa_settings_logged_out() {
-		wp_logout();
-
-		$this->assertFalse( Two_Factor_Core::maybe_force_2fa_settings() );
-	}
-
-	/**
-	 * @covers Two_Factor_Core::maybe_force_2fa_settings
-	 */
-	public function test_maybe_force_2fa_settings_is_rest() {
-		define( 'REST_REQUEST', true );
-
-		$this->assertFalse( Two_Factor_Core::maybe_force_2fa_settings() );
-	}
-
-	/**
-	 * @covers Two_Factor_Core::maybe_force_2fa_settings
-	 */
-	public function test_maybe_force_2fa_settings_is_ajax() {
-		define( 'DOING_AJAX', true );
-
-		$this->assertFalse( Two_Factor_Core::maybe_force_2fa_settings() );
-	}
-
-	/**
-	 * @covers Two_Factor_Core::is_two_factor_forced
-	 */
-	public function test_is_two_factor_forced_universal_option() {
-		update_site_option( Two_Factor_Core::FORCED_SITE_META_KEY, 1 );
-
-		$this->assertTrue( Two_Factor_Core::is_two_factor_forced( 123456 ) );
-	}
-
-	/**
-	 * @covers Two_Factor_Core::is_two_factor_forced
-	 */
-	public function test_is_two_factor_forced_non_existant_user() {
-		update_site_option( Two_Factor_Core::FORCED_SITE_META_KEY, 0 );
-
-		$this->assertFalse( Two_Factor_Core::is_two_factor_forced( 123456 ) );
-	}
-
-	/**
-	 * @covers Two_Factor_Core::is_two_factor_forced
-	 */
-	public function test_is_two_factor_forced_different_role() {
-		// Set role-based value to editors and adminstrators.
-		update_site_option( Two_Factor_Core::FORCED_ROLES_META_KEY, [ 'editor', 'administrator' ] );
-
-		$user = new WP_User( $this->factory->user->create( [ 'role' => 'author' ] ) );
-		wp_set_current_user( $user->ID );
-
-		$this->assertFalse( Two_Factor_Core::is_two_factor_forced( $user->ID ) );
-	}
-
-	/**
-	 * @covers Two_Factor_Core::is_two_factor_forced
-	 */
-	public function test_is_two_factor_forced_captured_role() {
-		// Set role-based value to editors and adminstrators.
-		update_site_option( Two_Factor_Core::FORCED_ROLES_META_KEY, [ 'editor', 'author' ] );
-
-		$user = new WP_User( $this->factory->user->create( [ 'role' => 'author' ] ) );
-		wp_set_current_user( $user->ID );
-
-		$this->assertTrue( Two_Factor_Core::is_two_factor_forced( $user->ID ) );
-	}
-
-	/**
-	 * @covers Two_Factor_Core::get_universally_forced_option
-	 */
-	public function test_get_universally_forced_option_multisite() {
-		// Set role-based value to editors and adminstrators.
-		update_site_option( Two_Factor_Core::FORCED_SITE_META_KEY, 1 );
-
-		$this->assertTrue( Two_Factor_Core::get_universally_forced_option() );
-	}
-
-	/**
-	 * @covers Two_Factor_Core::get_forced_user_roles
-	 */
-	public function test_get_forced_user_roles_multisite() {
-		// Set role-based value to editors and adminstrators.
-		update_site_option( Two_Factor_Core::FORCED_ROLES_META_KEY, [ 'author', 'editor', 'administrator' ] );
-
-		$this->assertEquals( [ 'author', 'editor', 'administrator' ], Two_Factor_Core::get_forced_user_roles() );
 	}
 }
--- two-factor.php
+++ two-factor.php
@@ -24,5 +24,7 @@ require_once( TWO_FACTOR_DIR . 'providers/class.two-factor-provider.php' );
  * Include the core that handles the common bits.
  */
 require_once( TWO_FACTOR_DIR . 'class.two-factor-core.php' );
+require_once( TWO_FACTOR_DIR . 'class.two-factor-force.php' );
 
 Two_Factor_Core::add_hooks();
+Two_Factor_Force::add_hooks();
