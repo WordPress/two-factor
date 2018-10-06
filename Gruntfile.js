802893b02a9f3d434fa1f131e9735b9ae16fef57
Kaspars Dambis <hi@kaspars.net>
2018-10-06 21:48:52 +0300

    Basic deploy tools

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,54 @@
 /* jshint es3:false, node:true */
 
+const fs = require( 'fs' );
+const ignoreParse = require( 'parse-gitignore' );
+
 module.exports = function( grunt ) {
 	'use strict';
 
-	grunt.initConfig( {
+	require( 'load-grunt-tasks' )( grunt );
 
+	let distignore = ignoreParse( fs.readFileSync( '.distignore' ) ).map( path => `!${path}` );
+
+	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' ),
 
-		// JavaScript linting with JSHint.
-		jshint: {
-			options: {
-				jshintrc: '.jshintrc'
-			},
-			all: [
-				'Gruntfile.js',
-				'js/*.js',
-				'!js/*.min.js'
-			]
+		dist_dir: 'dist',
+
+		clean: {
+			build: [ '<%= dist_dir %>' ],
 		},
 
+		copy: {
+			dist: {
+				src: [ '**' ].concat( distignore ),
+				dest: '<%= dist_dir %>',
+				expand: true,
+			}
+		},
+
+		wp_deploy: {
+			options: {
+				plugin_slug: 'two-factor',
+				build_dir: '<%= dist_dir %>',
+				assets_dir: 'assets',
+				deploy_tag: false,
+			},
+		},
 	} );
 
-	// Load tasks
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.registerTask(
+		'build', [
+			'clean',
+			'copy',
+		]
+	);
 
-	// Register tasks
-	grunt.registerTask( 'default', [
-		'jshint'
-	] );
+	grunt.registerTask(
+		'deploy', [
+			'build',
+			'wp_deploy',
+		]
+	);
 
 };
