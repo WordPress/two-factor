b863a3b352fea411fb3bbc440e91f834a465b38c
Kaspars Dambis <hi@kaspars.net>
2018-11-06 10:23:15 +0200

    Tag as 0.3.0
    Form action URL is potentially a breaking change.

--- two-factor.php
+++ two-factor.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/georgestephanis/two-factor/
  * Description: A prototype extensible core to enable Two-Factor Authentication.
  * Author: George Stephanis
- * Version: 0.2.0
+ * Version: 0.3.0
  * Author URI: https://stephanis.info
  * Network: True
  * Text Domain: two-factor
