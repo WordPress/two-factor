2fc4085875c9213886800ac049d1529baa6b0986
Office <office@belle-enterprises.com>
2018-11-28 16:46:11 +0100

    * remove unnecessary console.log * remove console.error that are output to the user via a normal error message to avoid duplicate error notifications * change console.log to .error when no error is output to user

--- providers/js/fido-u2f-login.js
+++ providers/js/fido-u2f-login.js
@@ -1,17 +1,13 @@
 /* global u2f, u2fL10n */
 ( function( $ ) {
 	if ( ! window.u2fL10n ) {
-		window.console.log( 'u2fL10n is not defined' );
+		window.console.error( 'u2fL10n is not defined' );
 		return;
 	}
 
-	window.console.log( 'sign', u2fL10n.request );
-
 	u2f.sign( u2fL10n.request[0].appId, u2fL10n.request[0].challenge, u2fL10n.request, function( data ) {
-		window.console.log( 'Authenticate callback', data );
-
 		if ( data.errorCode ) {
-			window.console.log( 'Registration Failed', data.errorCode );
+			window.console.error( 'Registration Failed', data.errorCode );
 		} else {
 			$( '#u2f_response' ).val( JSON.stringify( data ) );
 			$( '#loginform' ).submit();
