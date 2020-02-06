a5a4ad7b6bd99e57b31d9ec64712af1dea072de1
Kuba Mikita <jakub@bracketspace.com>
2020-02-06 10:55:03 +0100

    Call an action when user is authenticated (#324)

--- class-two-factor-core.php
+++ class-two-factor-core.php
@@ -702,6 +702,8 @@ class Two_Factor_Core {
 
 		wp_set_auth_cookie( $user->ID, $rememberme );
 
+		do_action( 'two_factor_user_authenticated', $user );
+
 		// Must be global because that's how login_header() uses it.
 		global $interim_login;
 		$interim_login = isset( $_REQUEST['interim-login'] ); // WPCS: override ok.
