f8d2fda9c4f381c4242e66abf62486e47116d2ef
Kaspars Dambis <hi@kaspars.net>
2020-05-04 12:53:00 +0300

    Escape the U2F key handle during lookup (#351)

--- providers/class.two-factor-fido-u2f.php
+++ providers/class.two-factor-fido-u2f.php
@@ -367,7 +367,12 @@ class Two_Factor_FIDO_U2F extends Two_Factor_Provider {
 		$query = $wpdb->prepare( "SELECT umeta_id FROM $table WHERE meta_key = '%s' AND user_id = %d", self::REGISTERED_KEY_USER_META_KEY, $user_id );
 
 		if ( $keyHandle ) {
-			$query .= $wpdb->prepare( ' AND meta_value LIKE %s', '%:"' . $keyHandle . '";s:%' );
+			$key_handle_lookup = sprintf( ':"%s";s:', $keyHandle ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.VariableNotSnakeCase
+
+			$query .= $wpdb->prepare(
+				' AND meta_value LIKE %s',
+				'%' . $wpdb->esc_like( $key_handle_lookup ) . '%'
+			);
 		}
 
 		$meta_ids = $wpdb->get_col( $query );
