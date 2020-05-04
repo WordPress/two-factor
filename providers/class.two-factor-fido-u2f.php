2d112fe0d4d0fcfbe794a799108ca1e051213e66
Kaspars Dambis <hi@kaspars.net>
2020-05-04 12:16:41 +0300

    Fix invalid trailing comma

--- providers/class.two-factor-fido-u2f.php
+++ providers/class.two-factor-fido-u2f.php
@@ -371,7 +371,7 @@ class Two_Factor_FIDO_U2F extends Two_Factor_Provider {
 
 			$query .= $wpdb->prepare(
 				' AND meta_value LIKE %s',
-				'%' . $wpdb->esc_like( $key_handle_lookup ) . '%',
+				'%' . $wpdb->esc_like( $key_handle_lookup ) . '%'
 			);
 		}
 
