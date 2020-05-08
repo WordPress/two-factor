c53a839c0d06760cf5a7445f199152639fcafff1
Kaspars Dambis <hi@kaspars.net>
2020-05-08 17:20:04 +0300

    Fix the boostrap logic

--- tests/docker/wp-dev-lib/entrypoint.sh
+++ tests/docker/wp-dev-lib/entrypoint.sh
@@ -6,8 +6,14 @@ export WP_TESTS_DIR="${WP_TESTS_DIR:-$WP_CORE_DIR/tests/phpunit}"
 # Start the MySQL server.
 service mysql start
 
-# Setup a test DB.
-mysql --user=root --password=root --execute="CREATE DATABASE wordpress_test;"
+# Setup the test DB.
+mysql --user=root --password=root --execute \
+	"GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+	FLUSH PRIVILEGES;
+	DROP DATABASE IF EXISTS wordpress_test;
+	CREATE DATABASE wordpress_test"
+
+mysqladmin --user=root --password=root password root
 
 # Run the command passed to this container.
 exec "$@"
--- tests/docker/wp-dev-lib/wp-tests-config.php
+++ tests/docker/wp-dev-lib/wp-tests-config.php
@@ -3,7 +3,7 @@
 define( 'DB_NAME', 'wordpress_test' );
 define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'root' );
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '127.0.0.1' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 
