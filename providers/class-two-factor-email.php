8c5584b6c0f2eddaf312944cb7cf21bd57331ffd
Kaspars Dambis <hi@kaspars.net>
2020-08-26 12:47:54 +0300

    Code might contain spaces — we’ll strip them away during processing

--- providers/class-two-factor-email.php
+++ providers/class-two-factor-email.php
@@ -269,7 +269,7 @@ class Two_Factor_Email extends Two_Factor_Provider {
 		<p><?php esc_html_e( 'A verification code has been sent to the email address associated with your account.', 'two-factor' ); ?></p>
 		<p>
 			<label for="authcode"><?php esc_html_e( 'Verification Code:', 'two-factor' ); ?></label>
-			<input type="tel" name="two-factor-email-code" id="authcode" class="input" value="" size="20" pattern="[0-9]*" />
+			<input type="tel" name="two-factor-email-code" id="authcode" class="input" value="" size="20" />
 			<?php submit_button( __( 'Log In', 'two-factor' ) ); ?>
 		</p>
 		<p class="two-factor-email-resend">
