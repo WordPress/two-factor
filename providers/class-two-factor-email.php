b14d9130234487d48f5559ea8dcdf1ecaa13f4ca
Dion Hulse <contact@dd32.id.au>
2023-02-14 13:54:54 +1000

    Include whitespace between the leading text and the actual input.

--- providers/class-two-factor-email.php
+++ providers/class-two-factor-email.php
@@ -267,6 +267,7 @@ class Two_Factor_Email extends Two_Factor_Provider {
 		require_once ABSPATH . '/wp-admin/includes/template.php';
 		?>
 		<p><?php esc_html_e( 'A verification code has been sent to the email address associated with your account.', 'two-factor' ); ?></p>
+		<br/>
 		<p>
 			<label for="authcode"><?php esc_html_e( 'Verification Code:', 'two-factor' ); ?></label>
 			<input type="tel" name="two-factor-email-code" id="authcode" class="input" value="" size="20" />
