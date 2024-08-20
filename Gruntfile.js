4a9feba82b81905e567de82a11249382b0f46b82
Dion Hulse <contact@dd32.id.au>
2024-08-20 18:17:36 +1000

    Remove the old wp_deploy code.

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,36 +47,12 @@ module.exports = function( grunt ) {
 				],
 			},
 		},
-
-		wp_deploy: {
-			options: {
-				plugin_slug: 'two-factor',
-				build_dir: '<%= dist_dir %>',
-				assets_dir: 'assets',
-			},
-			wporg: {
-				options: {
-					skip_confirmation: isTruthy( process.env.DEPLOY_SKIP_CONFIRMATION ),
-					svn_user: process.env.DEPLOY_SVN_USERNAME,
-					deploy_tag: isTruthy( process.env.DEPLOY_TAG ),
-					deploy_trunk: isTruthy( process.env.DEPLOY_TRUNK ),
-					assets_dir: ( isTruthy( process.env.DEPLOY_TAG ) || isTruthy( process.env.DEPLOY_TRUNK ) ) ? 'assets' : null,
-				},
-			},
-		},
 	} );
 
 	grunt.registerTask(
 		'build', [
 			'clean',
 			'copy',
-		]
-	);
-
-	grunt.registerTask(
-		'deploy', [
-			'build',
-			'wp_deploy',
 		]
 	);
 };
