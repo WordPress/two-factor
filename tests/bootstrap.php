4ca7561f7fd9d795934976cc06381f569e53c822
Dion Hulse <contact@dd32.id.au>
2024-08-20 16:10:21 +1000

    Import tests_add_filter() via the same method as Gutenberg: https://github.com/WordPress/gutenberg/blob/9c9f6535cdc3cd57a3b8583ea6292a2c90552ab3/phpunit/bootstrap.php#L41

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,8 +11,25 @@
 // Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available.
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
+// Determine the tests directory (from a WP dev checkout).
+// Try the WP_TESTS_DIR environment variable first.
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+// See if we're installed inside an existing WP dev instance.
+if ( ! $_tests_dir ) {
+	$_try_tests_dir = __DIR__ . '/../../../../../tests/phpunit';
+	if ( file_exists( $_try_tests_dir . '/includes/functions.php' ) ) {
+		$_tests_dir = $_try_tests_dir;
+	}
+}
+
+// Fallback.
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
 // Give access to tests_add_filter() function.
-require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
+require_once $_tests_dir . '/includes/functions.php';
 require_once dirname( __DIR__ ) . '/includes/function.login-header.php';
 require_once dirname( __DIR__ ) . '/includes/function.login-footer.php';
 
@@ -25,4 +42,4 @@ tests_add_filter(
 );
 
 // Start up the WP testing environment.
-require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
+require_once $_tests_dir . '/includes/bootstrap.php';
