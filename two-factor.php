e0b7ffd55fbbe4dd599341457b32184872a83086
Dion Hulse <dion@wordpress.org>
2024-08-22 12:56:30 +1000

    Bump minimum WP to 6.3, minimum PHP to 7.2. (#625)
    * Bump minimum WP to 6.3, minimum PHP to 7.2.24.
    
    * Composer: Update minimum PHP requirement.

--- two-factor.php
+++ two-factor.php
@@ -12,8 +12,8 @@
  * Plugin URI:        https://wordpress.org/plugins/two-factor/
  * Description:       Enable Two-Factor Authentication using time-based one-time passwords, Universal 2nd Factor (FIDO U2F, YubiKey), email, and backup verification codes.
  * Version:           0.9.1
- * Requires at least: 4.6
- * Requires PHP:      5.6
+ * Requires at least: 6.3
+ * Requires PHP:      7.2
  * Author:            Plugin Contributors
  * Author URI:        https://github.com/wordpress/two-factor/graphs/contributors
  * License:           GPL-2.0-or-later
