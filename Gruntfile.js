c2be5cc1f238ddaac94592947c73d4632e6efc7e
Dion Hulse <contact@dd32.id.au>
2024-08-22 13:08:05 +1000

    Remove old grunt deploy related code. See #543

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,16 +11,6 @@ module.exports = function( grunt ) {
 		invert: true,
 	} );
 
-	/**
-	 * Check if CLI input appears to indicate a truthy value.
-	 *
-	 * @param {string} input Value to check.
-	 * @return {boolean} If value appears to be truthy.
-	 */
-	function isTruthy( input ) {
-		return ( '1' === input || 'true' === input );
-	}
-
 	grunt.initConfig( {
 		pkg: grunt.file.readJSON( 'package.json' ),
 
@@ -47,23 +37,6 @@ module.exports = function( grunt ) {
 				],
 			},
 		},
-
-		wp_deploy: {
-			options: {
-				plugin_slug: 'two-factor',
-				build_dir: '<%= dist_dir %>',
-				assets_dir: 'assets',
-			},
-			wporg: {
-				options: {
-					skip_confirmation: isTruthy( process.env.DEPLOY_SKIP_CONFIRMATION ),
-					svn_user: process.env.DEPLOY_SVN_USERNAME,
-					deploy_tag: isTruthy( process.env.DEPLOY_TAG ),
-					deploy_trunk: isTruthy( process.env.DEPLOY_TRUNK ),
-					assets_dir: ( isTruthy( process.env.DEPLOY_TAG ) || isTruthy( process.env.DEPLOY_TRUNK ) ) ? 'assets' : null,
-				},
-			},
-		},
 	} );
 
 	grunt.registerTask(
@@ -73,10 +46,4 @@ module.exports = function( grunt ) {
 		]
 	);
 
-	grunt.registerTask(
-		'deploy', [
-			'build',
-			'wp_deploy',
-		]
-	);
 };
