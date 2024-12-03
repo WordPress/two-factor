93438bec0b682d80dfba72dd4752584ff3490ba0
Eric Mann <eric@eamann.com>
2024-12-03 11:32:50 -0800

    Port over the RFC reference tests

--- providers/class-two-factor-totp.php
+++ providers/class-two-factor-totp.php
@@ -52,6 +52,31 @@ class Two_Factor_Totp extends Two_Factor_Provider {
 	}
 
 	/**
+	 * Timestamp returned by time()
+	 *
+	 * @var int $now
+	 */
+	private static $now;
+
+	/**
+	 * Override time() in the current object for testing.
+	 *
+	 * @return int
+	 */
+	private static function time() {
+		return self::$now ?: time();
+	}
+
+	/**
+	 * Set up the internal state of time() invocations for deterministic generation.
+	 *
+	 * @param int $now Timestamp to use when overriding time().
+	 */
+	public static function __set_time( $now ) {
+		self::$now = $now;
+	}
+
+	/**
 	 * Register the rest-api endpoints required for this provider.
 	 *
 	 * @codeCoverageIgnore
@@ -566,7 +591,7 @@ class Two_Factor_Totp extends Two_Factor_Provider {
 		$ticks = range( - $max_ticks, $max_ticks );
 		usort( $ticks, array( __CLASS__, 'abssort' ) );
 
-		$time = floor( time() / self::DEFAULT_TIME_STEP_SEC );
+		$time = floor( self::time() / self::DEFAULT_TIME_STEP_SEC );
 
 		$digits = strlen( $authcode );
 
@@ -680,7 +705,7 @@ class Two_Factor_Totp extends Two_Factor_Provider {
 		}		
 
 		if ( false === $step_count ) {
-			$step_count = floor( time() / $time_step );
+			$step_count = floor( self::time() / $time_step );
 		}
 
 		$timestamp = self::pack64( $step_count );
