eebd754ca076a3dd608aaf14048e0531723377d0
Kaspars Dambis <hi@kaspars.net>
2025-01-09 14:15:23 +0200

    Bump the version

--- two-factor.php
+++ two-factor.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Two Factor
  * Plugin URI:        https://wordpress.org/plugins/two-factor/
  * Description:       Enable Two-Factor Authentication using time-based one-time passwords, Universal 2nd Factor (FIDO U2F, YubiKey), email, and backup verification codes.
- * Version:           0.10.0
+ * Version:           0.11.0
  * Requires at least: 6.3
  * Requires PHP:      7.2
  * Author:            WordPress.org Contributors
@@ -30,7 +30,7 @@ define( 'TWO_FACTOR_DIR', plugin_dir_path( __FILE__ ) );
 /**
  * Version of the plugin.
  */
-define( 'TWO_FACTOR_VERSION', '0.10.0' );
+define( 'TWO_FACTOR_VERSION', '0.11.0' );
 
 /**
  * Include the base class here, so that other plugins can also extend it.
