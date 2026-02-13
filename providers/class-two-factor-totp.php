f13b5cc58eb4bfb85284d03d664138c6961838f8
Kaspars Dambis <hi@kaspars.net>
2026-02-13 14:12:09 +0200

    Make it shorter and to the point

--- providers/class-two-factor-totp.php
+++ providers/class-two-factor-totp.php
@@ -357,7 +357,7 @@ class Two_Factor_Totp extends Two_Factor_Provider {
 						<?php
 						printf(
 							/* translators: 1: server date and time */
-							esc_html__( 'If the authentication code is rejected, please verify that the time reported by your server is accurate: %1$s. It is used for generating the authentication code and must match between the server and your device.', 'two-factor' ),
+							esc_html__( 'If the code is rejected, check that your web server time is accurate: %1$s. Your device and server times much match.', 'two-factor' ),
 							sprintf(
 								'<time class="two-factor-server-datetime-epoch" datetime="%1$s">%2$s (%3$s)</time>',
 								esc_attr( wp_date( 'c' ) ),
