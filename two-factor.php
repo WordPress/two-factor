12c033f7b7ac8a1b561e1b32aa944a0e6856fff5
Kaspars Dambis <hi@kaspars.net>
2026-09-26 12:12:23 +0300

    we’re ready for 0.17.0

--- two-factor.php
+++ two-factor.php
@@ -12,7 +12,7 @@
  * Plugin URI:        https://wordpress.org/plugins/two-factor/
  * Description:       Enable Two-Factor Authentication using time-based one-time passwords, email, and backup verification codes.
  * Requires at least: 7.0
- * Version:           0.17.0-rc
+ * Version:           0.17.0
  * Requires PHP:      7.4
  * Author:            WordPress.org Contributors
  * Author URI:        https://github.com/wordpress/two-factor/graphs/contributors
@@ -145,7 +145,7 @@ function two_factor_get_enabled_providers_option() {
  * This filter receives providers in core format: classname => path.
  *
  * @since 0.16
- * 
+ *
  * @param array $providers Registered providers in classname => path format.
  * @return array Filtered list of enabled providers.
  */
